459af2140cfe00e7ec5659f24e7b03becb147aac
Elizar Pepino <jupenz@gmail.com>
2013-10-07 17:46:18 +0800

    updated some shit

--- js/mine.js
+++ js/mine.js
@@ -6,12 +6,12 @@
     w.Mine = Mine;
   }
 
-  Mine.start = function(r, c){
+  Mine.start = function(r, c) {
     Mine.board = [];
     for (var x = 0; x < r; x++) {
-      var _row = []
+      var _row = [];
       for (var y = 0; y < c; y++) {
-        var _column = Math.round(Math.random()*2) === 0 ? 0 : 1;
+        var _column = Math.round(Math.random() * 2) === 0 ? 0 : 1;
         _row.push(_column);
       }
       console.log(_row);
@@ -28,7 +28,7 @@
    *
    */
   Mine.select = function(r, c) {
-    var result =  _getNumberOfMines(r, c);
+    var result = _getNumberOfMines(r, c);
     if (result === 'FAIL') {
       console.log('KABOOOOMMMM!!!!! MISSION FAIL!');
       return;
@@ -45,6 +45,7 @@
    * @api private
    *
    */
+
   function _getNumberOfMines(r, c) {
     var _val = Mine.board[r][c];
     if (_val === 0) {
@@ -52,20 +53,20 @@
     }
 
     var _numOfMines = 0,
-        _rBound = r === 0 ? true : false,
-        _cBound = c === 0 ? true : false,
-        _rBoundMax = r === Mine.board.length -1 ? true : false,
-        _cBoundMax = c === Mine.board[r].length ? true : false;
+      _rBound = r === 0 ? true : false,
+      _cBound = c === 0 ? true : false,
+      _rBoundMax = r === Mine.board.length - 1 ? true : false,
+      _cBoundMax = c === Mine.board[r].length ? true : false;
 
     /*
      * Setup Positions and Padding
      */
     var _point = {},
-        _padding = {};
-        _point.y = _rBound ? r : r - 1;
-        _point.x = _cBound ? c : c - 1;
-        _padding.y = _point.y + ((_rBound || _rBoundMax) ? 1 : 2);
-        _padding.x = _point.x + ((_cBound || _cBoundMax) ? 1 : 2);
+      _padding = {};
+    _point.y = _rBound ? r : r - 1;
+    _point.x = _cBound ? c : c - 1;
+    _padding.y = _point.y + ((_rBound || _rBoundMax) ? 1 : 2);
+    _padding.x = _point.x + ((_cBound || _cBoundMax) ? 1 : 2);
 
     for (var y = _point.y; y <= _padding.y; y++) {
       for (var x = _point.x; x <= _padding.x; x++) {
@@ -80,9 +81,9 @@
   }
 
   w.onload = function() {
-    if (document.readyState === 'complete') {
+    if (w.document.readyState === 'complete') {
       Mine.start(10, 10);
     }
   };
 
-})(window, undefined);
+})(window);
